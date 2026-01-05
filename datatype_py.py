bbbc7854ee8a97395077f5c31292874d1e0c0114
chavdayash22 <chavdayash6282@gmail.com>
2026-01-05 10:52:20 +0530

    Add files via upload

---
+++ datatype_py.py
@@ -0,0 +1,16 @@
+"""x = "hello world"
+print(x)"""
+"""x = 20.5
+print(x)"""
+"""x = 1j
+print(x)"""
+"""x = true 
+print(x)"""
+"""x = ["apple","banana","cherry"]
+print(x)"""
+"""x = ["apple","banana","cherry"]
+print(x)"""
+"""x = ["NAME :","john","age",36]
+print(x)"""
+x = ?
+print(x)

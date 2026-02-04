386ed1255aad5933a3804601ca488bd5fd37c399
chavdayash22 <chavdayash6282@gmail.com>
2026-02-04 08:03:50 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,54 @@
+#output using print()
+
+# 1. int example
+"""a = 22
+print(a)
+b = 33
+print(b)"""
+
+# 2 float example
+"""b = 3.3
+print(b)
+c = 3.45
+print(c)"""
+
+# 3 boolean example
+"""a = True
+print(a)
+b = False
+print(b)"""
+
+# 4 str example
+"""x = "hello"
+print(x)
+x = "good bye"
+print(x)"""
+
+#input using input()
+
+#1 int example
+"""number = int(input("enter  number :"))
+print("number is:",number)
+number = int(input("enter number :"))
+print("number is",number)"""
+
+#2 float example
+
+"""price = float(input("enter price"))
+print("price is ",price)
+price = float(input("enter price"))
+print("price is",price)"""
+
+# 3 boolean example
+
+"""Q = boolean(input("this is true"))
+print(Q)
+Q = boolean(input("thid is false"))
+print(Q)"""
+
+# 4 str example
+
+name = str(input("enter your name"))
+print("hello",name)
+name = str(input("enter your name"))
+print("hello",name)
